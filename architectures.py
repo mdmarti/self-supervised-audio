ce99a4294e81ac33b63ff460822168caacfebd13
mdmarti <miles.martinez@duke.edu>
2024-06-04 13:12:35 -0400

    updating architectures

--- architectures.py
+++ architectures.py
@@ -7,320 +7,183 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-#### All below code from https://github.com/facebookresearch/vicreg/tree/main
+#### From torch's documentation:
 
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(
-        in_planes,
-        out_planes,
-        kernel_size=3,
+### size of 1d conv output will be:
+# L_out = floor([L_in + 2 x pad - dilation x (kernel - 1) - 1]/[stride] + 1)
+
+def conv1d(in_channels,out_channels,kernel_size,stride=1,dilation=1):
+
+    return nn.conv1d(
+        in_channels,
+        out_channels,
+        kernel_size=kernel_size,
         stride=stride,
-        padding=dilation,
-        groups=groups,
-        bias=False,
         dilation=dilation,
+        bias=False,
+        group=1,
+        padding=dilation
     )
 
 
-def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-    __constants__ = ["downsample"]
+class Encoder(nn.Module):
 
     def __init__(
-        self,
-        inplanes,
-        planes,
-        stride=1,
-        downsample=None,
-        groups=1,
-        base_width=64,
-        dilation=1,
-        norm_layer=None,
-        last_activation="relu",
+            self,
+            in_channels,
+            dilations=[2,4,8,8,4],
+            strides=[5,4,2,2,2],
+            kernels=[10,8,4,4,4],
+            encode_dim=32,
+            norm_layer=None,
     ):
-        super(BasicBlock, self).__init__()
+        super(Encoder,self).__init__()
         if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        if groups != 1 or base_width != 64:
-            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
-        if dilation > 1:
-            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
-        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = norm_layer(planes)
-        self.downsample = downsample
-        self.stride = stride
+            norm_layer = nn.BatchNorm1d
 
-    def forward(self, x):
-        identity = x
+        self.encode_dim = encode_dim
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+        encode_dim /= 2
 
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-    __constants__ = ["downsample"]
-
-    def __init__(
-        self,
-        inplanes,
-        planes,
-        stride=1,
-        downsample=None,
-        groups=1,
-        base_width=64,
-        dilation=1,
-        norm_layer=None,
-        last_activation="relu",
-    ):
-        super(Bottleneck, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        width = int(planes * (base_width / 64.0)) * groups
-        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
-        self.conv1 = conv1x1(inplanes, width)
-        self.bn1 = norm_layer(width)
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
-        self.bn2 = norm_layer(width)
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        self.bn3 = norm_layer(planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-        if last_activation == "relu":
-            self.last_activation = nn.ReLU(inplace=True)
-        elif last_activation == "none":
-            self.last_activation = nn.Identity()
-        elif last_activation == "sigmoid":
-            self.last_activation = nn.Sigmoid()
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            identity = self.downsample(x)
-
-        out += identity
-        out = self.last_activation(out)
-
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(
-        self,
-        block,
-        layers,
-        num_channels=3,
-        zero_init_residual=False,
-        groups=1,
-        widen=1,
-        width_per_group=64,
-        replace_stride_with_dilation=None,
-        norm_layer=None,
-        last_activation="relu",
-    ):
-        super(ResNet, self).__init__()
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
-        # self._last_activation = last_activation
-
-        self.padding = nn.ConstantPad2d(1, 0.0)
-
-        self.inplanes = width_per_group * widen
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError(
-                "replace_stride_with_dilation should be None "
-                "or a 3-element tuple, got {}".format(replace_stride_with_dilation)
+        self.strided_encoder = nn.Sequential(
+            conv1d(in_channels, # downsample to len 87
+                   out_channels=encode_dim,
+                   kernel_size=kernels[0],
+                   stride=strides[0],
+                   dilation=1),
+                   norm_layer,
+            conv1d(in_channels, # downsample to len 21
+                   out_channels=encode_dim,
+                   kernel_size=kernels[1],
+                   stride=strides[1],
+                   dilation=1),
+                   norm_layer,
+            conv1d(in_channels, # downsample to len 10
+                   out_channels=encode_dim,
+                   kernel_size=kernels[2],
+                   stride=strides[2],
+                   dilation=1),
+                   norm_layer,
+            conv1d(in_channels, # downsample to len 4
+                   out_channels=encode_dim,
+                   kernel_size=kernels[3],
+                   stride=strides[3],
+                   dilation=1),
+                   norm_layer,
+            conv1d(in_channels, #downsample to len 1
+                   out_channels=encode_dim,
+                   kernel_size=kernels[4],
+                   stride=strides[4],
+                   dilation=1),
+                   norm_layer,
             )
-        self.groups = groups
-        self.base_width = width_per_group
 
-        # change padding 3 -> 2 compared to original torchvision code because added a padding layer
-        num_out_filters = width_per_group * widen
-        self.conv1 = nn.Conv2d(
-            num_channels,
-            num_out_filters,
-            kernel_size=7,
-            stride=2,
-            padding=2,
-            bias=False,
-        )
-        self.bn1 = norm_layer(num_out_filters)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, num_out_filters, layers[0])
-        num_out_filters *= 2
-        self.layer2 = self._make_layer(
-            block,
-            num_out_filters,
-            layers[1],
-            stride=2,
-            dilate=replace_stride_with_dilation[0],
-        )
-        num_out_filters *= 2
-        self.layer3 = self._make_layer(
-            block,
-            num_out_filters,
-            layers[2],
-            stride=2,
-            dilate=replace_stride_with_dilation[1],
-        )
-        num_out_filters *= 2
-        self.layer4 = self._make_layer(
-            block,
-            num_out_filters,
-            layers[3],
-            stride=2,
-            dilate=replace_stride_with_dilation[2],
-            last_activation=last_activation,
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+        self.dilated_encoder=nn.Sequential(
+             conv1d(in_channels, # downsample to len 87
+                   out_channels=encode_dim,
+                   kernel_size=kernels[0],
+                   stride=3,
+                   dilation=dilations[0]),
+                   norm_layer,
+            conv1d(in_channels, # downsample to len 21
+                   out_channels=encode_dim,
+                   kernel_size=kernels[1],
+                   stride=3,
+                   dilation=dilations[1]),
+                   norm_layer,
+            conv1d(in_channels, # downsample to len 10
+                   out_channels=encode_dim,
+                   kernel_size=kernels[2],
+                   stride=2,
+                   dilation=dilations[2]),
+                   norm_layer,
+            conv1d(in_channels, # downsample to len 4
+                   out_channels=encode_dim,
+                   kernel_size=kernels[3],
+                   stride=2,
+                   dilation=dilations[3]),
+                   norm_layer,
+            conv1d(in_channels, #downsample to len 1
+                   out_channels=encode_dim,
+                   kernel_size=kernels[4],
+                   stride=3,
+                   dilation=dilations[4]),
+                   norm_layer,
+            )
+        
+        def _weights_init(m):
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            if isinstance(m, nn.Conv1d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm1d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
+        self.apply(_weights_init)
 
-    def _make_layer(
-        self, block, planes, blocks, stride=1, dilate=False, last_activation="relu"
-    ):
-        norm_layer = self._norm_layer
-        downsample = None
-        previous_dilation = self.dilation
-        if dilate:
-            self.dilation *= stride
-            stride = 1
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
-            )
+        def forward(self,x):
 
-        layers = []
-        layers.append(
-            block(
-                self.inplanes,
-                planes,
-                stride,
-                downsample,
-                self.groups,
-                self.base_width,
-                previous_dilation,
-                norm_layer,
-                last_activation=(last_activation if blocks == 1 else "relu"),
-            )
-        )
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(
-                block(
-                    self.inplanes,
-                    planes,
-                    groups=self.groups,
-                    base_width=self.base_width,
-                    dilation=self.dilation,
-                    norm_layer=norm_layer,
-                    last_activation=(last_activation if i == blocks - 1 else "relu"),
-                )
-            )
+            dilatedRep,stridedRep = self.dilated_encoder(x),self.strided_encoder(x)
+            rep = torch.cat([dilatedRep,stridedRep],dim=-1)
+            return rep
+        
+def Projector(sizes):
 
-        return nn.Sequential(*layers)
+    layers = []
+    for ii in range(len(sizes) - 2):
 
-    def forward(self, x):
-        x = self.padding(x)
+        layers.append(nn.Linear(sizes[ii],sizes[ii+1]))
+        layers.append(nn.BatchNorm1d(sizes[ii+1]))
+        layers.append(nn.ReLU(True))
+    layers.append(nn.Linear(sizes[-2],sizes[-1],bias=False))
+    return nn.Sequential(*layers)
 
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+class VicRegWaves(nn.Module):
 
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+    def __init__(self,args):
+        super().__init__()
+        self.args = args 
+        self.encoder = Encoder(in_channels=1,
+                               dilations=args.dilations,
+                               strides=args.strides,
+                               kernels=args.kernels,
+                               encode_dim=args.encode_dim,
+                               norm_lays=nn.BatchNorm1d)
+        self.projector = Projector(args.sizes)
+        self.latent_dim=args.encode_dim
 
-        return x
+    def forward(self,x1,x2):
 
+        z1 = self.encoder(x1)
+        z2 = self.encoder(x2)
+        z2Hat = self.projector(z1)
 
-def resnet34(**kwargs):
-    return ResNet(BasicBlock, [3, 4, 6, 3], **kwargs), 512
+        repr_loss = F.mse_loss(z2Hat,z2)
+        
+        centeredZ2 = z2 - z2.mean(dim=0)
+        centeredZ2hat = z1 - z1.mean(dim=0)
 
+        std_z2 = torch.sqrt(centeredZ2.var(dim=0) + 1e-4)
+        std_z2hat = torch.sqrt(centeredZ2hat.var(dim=0) + 1e-4)
 
-def resnet50(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 6, 3], **kwargs), 2048
+        std_loss = torch.mean(F.relu(1 - std_z2))/2 + torch.mean(F.relu(1-std_z2hat))/2
+        
+        cov_z2 = centeredZ2.T @ centeredZ2 / centeredZ2.shape[0]
+        cov_z2hat = centeredZ2hat.T @ centeredZ2hat / centeredZ2hat.shape[0]
+        
+        cov_loss = off_diagonal(cov_z2).pow_(2).sum().div(self.latent_dim) + \
+            off_diagonal(cov_z2hat).pow_(2).sum().div(self.latent_dim)
 
-
-def resnet101(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 23, 3], **kwargs), 2048
-
-
-def resnet50x2(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 6, 3], widen=2, **kwargs), 4096
-
-
-def resnet50x4(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 6, 3], widen=4, **kwargs), 8192
-
-
-def resnet50x5(**kwargs):
-    return ResNet(Bottleneck, [3, 4, 6, 3], widen=5, **kwargs), 10240
-
-
-def resnet200x2(**kwargs):
-    return ResNet(Bottleneck, [3, 24, 36, 3], widen=2, **kwargs), 4096
+        loss = (self.args.sim_coeff*repr_loss 
+                + self.args.std_coeff * std_loss 
+                + self.args.cov_coeff * cov_loss)
+        
+        return loss 
+    
+def off_diagonal(x):
+    n, m = x.shape
+    assert n == m
+    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
